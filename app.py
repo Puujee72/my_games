c5bb0c160c076886704d6f8de3d531bb83c3e064
Puujee <puujee1@gmail.com>
2025-07-03 16:54:25 +0800

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
 import os
 
+# Flask апп үүсгэх
+app = Flask(__name__)
+
+# Сешн ашиглахын тулд secret_key заавал хэрэгтэй
+# Үүнийг аюулгүй байлгахын тулд орчин (env) хувьсагчаар авах нь зөв
+app.secret_key = os.environ.get('SECRET_KEY', 'your_very_secure_and_random_secret_key_here_12345')
+
 # Games package дотроос blueprint-үүдийг импортлох
 from games.guess_number import guess_number_bp
 from games.rps import rps_bp
@@ -8,18 +15,14 @@ from games.guess_image import guess_image_bp
 from games.fibonacci import fibonacci_bp
 from games.maze import maze_bp
 
-app = Flask(__name__)
-# Сешн ашиглахын тулд secret_key заавал байх ёстой. Үүнийг **нууцлагдсан, санамсаргүй урт тэмдэгтүүд**-ээр солино уу!
-app.secret_key = 'your_very_secure_and_random_secret_key_here_12345'
-
-# Blueprint-үүдийг үндсэн аппликейшнд бүртгэх
+# Blueprint-үүдийг үндсэн апп-д бүртгэх
 app.register_blueprint(guess_number_bp, url_prefix='/guess_number')
 app.register_blueprint(rps_bp, url_prefix='/rps')
 app.register_blueprint(guess_image_bp, url_prefix='/guess_image')
 app.register_blueprint(fibonacci_bp, url_prefix='/fibonacci')
 app.register_blueprint(maze_bp, url_prefix='/maze')
 
-# Үндсэн хуудас - Тоглоомуудын жагсаалт
+# Үндсэн хуудас - тоглоомуудын жагсаалт
 @app.route('/')
 def index():
     games = [
@@ -31,5 +34,6 @@ def index():
     ]
     return render_template('index.html', games=games)
 
+# Локал хөгжүүлэлтийн үед ажиллах
 if __name__ == '__main__':
     app.run(debug=True)
